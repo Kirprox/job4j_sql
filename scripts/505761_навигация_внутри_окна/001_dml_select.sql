91a3d9beed6e956c3e7800f2c6b9e7da4ac51377
Kir <kirpro95@yandex.ru>
2026-08-05 10:37:14 +0300

    2.3.9.3. Навигация внутри окна: FIRST_VALUE(), LAST_VALUE() и NTH_VALUE() [#505761]

---
+++ scripts/505761_навигация_внутри_окна/001_dml_select.sql
@@ -0,0 +1,36 @@
+WITH order_totals AS (
+    SELECT
+	o.user_id,
+	o.id AS order_id,
+	o.created_at,
+	SUM(oi.quantity * oi.unit_price) AS order_amount
+    FROM orders AS o
+    JOIN order_items AS oi ON oi.order_id = o.id
+    GROUP BY
+	o.id,
+	o.user_id,
+	o.created_at
+)
+SELECT
+    user_id,
+    order_id,
+    created_at,
+    order_amount,
+    FIRST_VALUE(order_amount) OVER (
+	PARTITION BY user_id
+	ORDER BY created_at
+    ) AS first_order_amount,
+    LAST_VALUE(order_amount) OVER (
+	PARTITION BY user_id
+	ORDER BY created_at
+	ROWS BETWEEN UNBOUNDED PRECEDING
+		AND UNBOUNDED FOLLOWING
+    ) AS last_order_amount,
+    NTH_VALUE(order_amount, 2) OVER (
+	PARTITION BY user_id
+	ORDER BY created_at
+	ROWS BETWEEN UNBOUNDED PRECEDING
+		AND UNBOUNDED FOLLOWING
+    ) AS second_order_amount
+FROM order_totals
+ORDER BY user_id, created_at;
